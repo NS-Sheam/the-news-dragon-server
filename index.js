fc73ede269a7debc0a67862e3aaec29250fd0f51
NS-Sheam <123sheamfeni@gmail.com>
2023-04-29 15:35:40 +0600

    vercel configuration file added

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const categories = require("./data/categories.json")
+const news = require("./data/news.json")
 
 app.use(cors())
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Dragon is running")
 })
-app.get("/categories", (req, res) =>{
+app.get("/categories", (req, res) => {
     res.send(categories);
 })
-app.listen(port, ()=>{
+// Load all news 
+app.get("/news", (req, res) => {
+    res.send(news);
+})
+// Load specific news 
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    // console.log(req.params.id);
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
+
+// Load with categoriwise 
+app.get("/categories/:id", (req, res) => {
+    const id = +req.params.id;
+    if (id === 0) {
+        res.send(news);
+    }
+    else {
+        const categoriesNews = news.filter(n => +n.category_id === id)
+        res.send(categoriesNews);
+    }
+})
+app.listen(port, () => {
     console.log(`Dragon Api is running on port ${port}`);
 })
